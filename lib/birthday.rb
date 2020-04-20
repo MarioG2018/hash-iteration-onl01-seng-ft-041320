b863d579602250a8a38cb868145fe5d2402e8080
Mario Garza <mario.garza02@utrgv.edu>
2020-04-20 19:00:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,6 +17,8 @@ def age_appropriate_birthday(birthday_kids)
      if(age<13)
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
      end
+     else
+       puts"You are too old for this"
   end
 end
 
